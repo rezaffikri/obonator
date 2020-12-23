5528f4a4fa7ed898dc1040f9319273127094985e
rezaffikri <rezaf.fikri@gmail.com>
2020-12-23 18:31:49 +0000

    Deploying to gh-pages from  @ 06e5d64283961f9c8e2e83f7ce18afb15db873fe 🚀

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -5,7 +5,7 @@ const swInstallEvent = 'install';
 const swInstalledEvent = 'installed';
 const swActivateEvent = 'activate';
 const staticCachePrefix = 'blazor-cache-v';
-const staticCacheName = 'blazor-cache-v8';
+const staticCacheName = 'blazor-cache-v9';
 const requiredFiles = [
 "/obonator/404.html",
 "/obonator/css/app.css",
