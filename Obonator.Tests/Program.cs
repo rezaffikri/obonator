526c22681a5f22deaa34ef2ca25c492144f3e7d2
Reza F. Fikri <rezaf.fikri@outlook.com>
2020-12-23 14:29:20 +0700

    update obon library

---
+++ Obonator.Tests/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Obonator.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Library.ObonCryptography.TripleDes.Encrypt("a2934fa2-6f7e-4ac9-8210-681814ac86c4", "rezaffikri:21122112:355651080329676:20200106123013:v1", false);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
