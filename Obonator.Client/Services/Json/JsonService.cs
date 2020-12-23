5e0a8cf8a7e187fbee3623f7ed38020ba2f43ad9
Reza F. Fikri <rezaf.fikri@outlook.com>
2020-12-23 22:03:36 +0700

    Remove all ARN Feature to Internal App Obonzilla     Update TripleDes     Add New Menu JSON Viewer     Add Validator to JSON Viewer     Add Beatufier JSON Viewer     Merge JSON Escape/Unescape to JSON Viewer     Add Bulma Again     Slowly Remove Obon CSS Framework And Use Custom CSS to this Project Only Base on Bulma CSS

---
+++ Obonator.Client/Services/Json/JsonService.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+
+namespace Obonator.Client.Services.Json
+{
+    public class JsonService
+    {
+        public string JsonValidator(string json)
+        {
+            string errorMsg = "";
+            try
+            {
+                JsonSerializer.Deserialize<JsonElement>(json);
+            }
+            catch (Exception ex)
+            {
+                int line = 1;
+                int position = 0;
+                string msg = "";
+                if (ex.Message.Contains("LineNumber: "))
+                {
+                    line = int.Parse(ex.Message.Substring(ex.Message.IndexOf("LineNumber: ") + 12, 3).Replace("|", "").Trim());
+                }
+                if (ex.Message.Contains("BytePositionInLine: "))
+                {
+                    position = int.Parse(ex.Message.Substring(ex.Message.IndexOf("BytePositionInLine: ") + 20).Replace(".", "".Trim()));
+                }
+                if (ex.Message.Contains("Expected"))
+                {
+                    msg = ex.Message.Substring(ex.Message.IndexOf("Expected")).Replace(".", "".Trim());
+                    int lengthMsg = msg.IndexOf("Path", StringComparison.Ordinal);
+                    msg = msg.Substring(0, lengthMsg).Trim();
+                }
+                string error;
+                if (json.Split("\n").Length > 1)
+                {
+                    error = json.Split("\n")[line];
+                }
+                else
+                {
+                    int startIndex = position - 20;
+                    int lengthMsg = 40;
+
+                    if (startIndex < 0)
+                    {
+                        startIndex = 0;
+                        lengthMsg = json.Length;
+                    }
+
+                    for (int i = 0; i < 5; i++)
+                    {
+                        int length = startIndex + lengthMsg;
+                        if (length > json.Length)
+                        {
+                            lengthMsg -= 8;
+                        }
+                    }
+                    error = json.Substring(startIndex, lengthMsg);
+                }
+                errorMsg = $@"Error in line {line + 1}, somewhere near: 
+                            {Environment.NewLine} {error} {Environment.NewLine} {msg}";
+            }
+            return errorMsg;
+        }
+
+        public string PrettyJson(string json)
+        {
+            string error = JsonValidator(json);
+            if (!string.IsNullOrEmpty(error))
+            {
+                return error;
+            }
+
+            var options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>(json);
+
+            return JsonSerializer.Serialize(jsonElement, options);
+        }
+
+        public string Escape(string json)
+        {
+            string error = JsonValidator(json);
+            if (!string.IsNullOrEmpty(error))
+            {
+                return error;
+            }
+
+            JsonSerializerOptions jsonSerializerOption = new JsonSerializerOptions();
+            jsonSerializerOption.Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+            return JsonSerializer.Serialize(json, jsonSerializerOption);
+        }
+        public string Unescape(string json)
+        {
+            string error = JsonValidator(json);
+            if (!string.IsNullOrEmpty(error))
+            {
+                return error;
+            }
+
+            JsonSerializerOptions jsonSerializerOption = new JsonSerializerOptions();
+            jsonSerializerOption.Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+            return JsonSerializer.Deserialize<string>(json, jsonSerializerOption);
+        }
+    }
+}
